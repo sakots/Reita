c7b37124edcd7f1d68cbf08630a2c212664292da
sakots <sakots8000@gmail.com>
2022-08-23 23:29:35 +0900

    del

--- reita/theme/monoreita_dev/react.blade.php
+++
@@ -1,7 +0,0 @@
-@section('react')
-        <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
-        <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-		<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
-		<!-- Load our React component. -->
-		<script src="templates/{{$themedir}}/js/index.js"></script>
-@show
