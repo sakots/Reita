936935676f93565f5fdd58d8f2bf9865181e30e2
sakots <sakots8000@gmail.com>
2024-04-29 01:56:08 +0900

    initでエラーのとき動かさないように

--- reita/frontend/src/App.tsx
+++ reita/frontend/src/App.tsx
@@ -7,10 +7,16 @@ import Reply from './pages/Reply'
 import Searches from './pages/Searches'
 import './App.css'
 
+const initDataURL = "https://localhost/dev/Reita/reita/backend/getInit.php"
 const boardDataURL = "https://localhost/dev/Reita/reita/backend/getConfig.php"
 
 const App = () => {
-
+  const [initData, setInitData] = useState("")
+  useEffect(() => {
+    axios.get(initDataURL).then((response) => {
+      setInitData(response.data);
+    });
+  }, []);
   const [boardData, setBoardData] = useState("")
   useEffect(() => {
     axios.get(boardDataURL).then((response) => {
@@ -18,18 +24,27 @@ const App = () => {
     });
   }, []);
 
-  return (
-    <>
-      <h1>{boardData.boardTitle}</h1>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path='catalog' element={<Catalog />} />
-        <Route path='Reply' element={<Reply />} />
-        <Route path='searches' element={<Searches />} />
-        <Route path='*' element={<h2>ページがないよ</h2>} />
-      </Routes>
-    </>
-  )
+  if (initData.flag === false) {
+    return (
+      <>
+        <h1>{boardData.boardTitle}</h1>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='catalog' element={<Catalog />} />
+          <Route path='Reply' element={<Reply />} />
+          <Route path='searches' element={<Searches />} />
+          <Route path='*' element={<h2>ページがないよ</h2>} />
+        </Routes>
+      </>
+    )
+  } else {
+    return (
+      <div className='initError'>
+        {initData.error}
+      </div>
+    )
+  }
+
 }
 
 export default App
