aad58950a60955a36d66da8bceb568222ed1b600
sakots <sakots8000@gmail.com>
2024-04-29 15:26:07 +0900

    分割

---
+++ reita/frontend/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const boardDataURL = "https://localhost/dev/Reita/reita/backend/getConfig.php"
+
+const Header = () => {
+
+  const [boardData, setBoardData] = useState("")
+  useEffect(() => {
+    axios.get(boardDataURL).then((response) => {
+      setBoardData(response.data);
+    });
+  }, []);
+
+  console.log(boardData)
+
+  return (
+    <h1>{boardData.boardTitle}</h1>
+  )
+}
+
+export default Header
